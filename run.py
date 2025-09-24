71a281ea85035737c6d13492b0606cb94499f7c3
EobardThawne2 <sumeerkhattar@gmail.com>
2025-09-24 22:54:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -14,4 +14,5 @@ app = create_app()
 if __name__ == "__main__":
     # Ensure the models directory exists
     os.makedirs(app.config['MODEL_PATH'], exist_ok=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
+
